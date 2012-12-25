e095141ab208e8a1747f9559dfd66428eb1287dc
misty_rc <misty.rc@gmail.com>
2012-12-25 17:00:27 +0900

    init commit

---
+++ src/TestApp.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: arai
+ * Date: 12/12/25
+ * Time: 16:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestApp {
+    public static void main(String[] args) {
+        System.out.println("hogehoge");
+    }
+}
---
+++ src/TestUtil.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: arai
+ * Date: 12/12/25
+ * Time: 16:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestUtil {
+    public static String getHoge() {
+        return "hoge";
+    }
+}
